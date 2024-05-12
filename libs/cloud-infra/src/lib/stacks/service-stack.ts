59b2181f534b0299582f9353ad96ecfdfcc600be
Blaine Interrante <interrante.blaine@gmail.com>
2024-05-12 16:17:04 -0400

    Deployed lambda works

--- libs/cloud-infra/src/lib/stacks/service-stack.ts
+++ libs/cloud-infra/src/lib/stacks/service-stack.ts
@@ -1,9 +1,12 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
 import { DynamoStack } from './dynamo-stack';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { join } from 'path';
 import { S3Stack } from './s3-stack';
+import {
+  Runtime,
+  IFunction,
+  Function,
+  AssetCode,
+} from 'aws-cdk-lib/aws-lambda';
 
 interface ServiceStackProps extends StackProps {
   dynamoStack: DynamoStack;
@@ -11,23 +14,22 @@ interface ServiceStackProps extends StackProps {
 }
 
 export class ServiceStack extends Stack {
-  //   public readonly fns: Map<string, string>;
+  public readonly WebServiceFn: IFunction;
 
   constructor(scope: App, id: string, props?: ServiceStackProps) {
     super(scope, id, props);
 
-    // const lambdaFunction = new NodejsFunction(this, 'MyLambdaFunction', {
-    //   runtime: Runtime.NODEJS_LATEST,
-    //   entry: join(__dirname, 'path/to/lambda/handler.ts'), // Path to your Lambda handler
-    //   handler: 'handler',
-    //   environment: {
-    //     TABLE_NAME: props.dynamoStack.table.tableName,
-    //     BUCKET_NAME: props.s3Stack.bucket.bucketName,
-    //   },
-    // });
+    this.WebServiceFn = new Function(this, 'WebServiceFn', {
+      runtime: Runtime.NODEJS_LATEST,
+      code: new AssetCode('./dist/apps/web-service-fn'),
+      handler: 'main.handler',
+      environment: {
+        TABLE_NAME: props.dynamoStack.table.tableName,
+        BUCKET_NAME: props.s3Stack.bucket.bucketName,
+      },
+    });
 
-    // // Grant the lambda function permissions to use the DynamoDB table and S3 bucket
-    // props.dynamoStack.table.grantReadWriteData(lambdaFunction);
-    // props.s3Stack.bucket.grantReadWrite(lambdaFunction);
+    props.dynamoStack.table.grantReadWriteData(this.WebServiceFn);
+    props.s3Stack.bucket.grantReadWrite(this.WebServiceFn);
   }
 }
