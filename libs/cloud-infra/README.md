88a8f75397696b22325d93d2ffcddc6c80335234
Blaine Interrante <interrante.blaine@gmail.com>
2024-05-12 12:37:27 -0400

    Integrate cdk commands as nx run commands

--- libs/cloud-infra/README.md
+++ libs/cloud-infra/README.md
@@ -8,4 +8,8 @@ Run `nx build cloud-infra` to build the library.
 
 Run `nx run cloud-infra:synth` to synthesize the CloudFormation Template.
 
-<!-- Run `nx run cloud-infra:deploy` to deploy the stack to AWS. -->
+Run `nx run cloud-infra:deploy <Stack1> <Stack2>` or `nx run cloud-infra:deploy --all` to deploy AWS stacks.
+
+Run `nx run cloud-infra:destroy <Stack1> <Stack2>` or `nx run cloud-infra:destroy --all` to destroy deployed AWS stacks.
+
+Run `nx run cloud-infra:bootstrap` to set up the initial resources required for managing your CDK deployments in AWS. This is necessary before your first deployment to prepare the AWS environment.
--- libs/cloud-infra/src/lib/stacks/service-stack.ts
+++ libs/cloud-infra/src/lib/stacks/service-stack.ts
@@ -11,23 +11,23 @@ interface ServiceStackProps extends StackProps {
 }
 
 export class ServiceStack extends Stack {
-  public readonly fns: Map<string, string>;
+  //   public readonly fns: Map<string, string>;
 
   constructor(scope: App, id: string, props?: ServiceStackProps) {
     super(scope, id, props);
 
-    const lambdaFunction = new NodejsFunction(this, 'MyLambdaFunction', {
-      runtime: Runtime.NODEJS_LATEST,
-      entry: join(__dirname, 'path/to/lambda/handler.ts'), // Path to your Lambda handler
-      handler: 'handler',
-      environment: {
-        TABLE_NAME: props.dynamoStack.table.tableName,
-        BUCKET_NAME: props.s3Stack.bucket.bucketName,
-      },
-    });
+    // const lambdaFunction = new NodejsFunction(this, 'MyLambdaFunction', {
+    //   runtime: Runtime.NODEJS_LATEST,
+    //   entry: join(__dirname, 'path/to/lambda/handler.ts'), // Path to your Lambda handler
+    //   handler: 'handler',
+    //   environment: {
+    //     TABLE_NAME: props.dynamoStack.table.tableName,
+    //     BUCKET_NAME: props.s3Stack.bucket.bucketName,
+    //   },
+    // });
 
-    // Grant the lambda function permissions to use the DynamoDB table and S3 bucket
-    props.dynamoStack.table.grantReadWriteData(lambdaFunction);
-    props.s3Stack.bucket.grantReadWrite(lambdaFunction);
+    // // Grant the lambda function permissions to use the DynamoDB table and S3 bucket
+    // props.dynamoStack.table.grantReadWriteData(lambdaFunction);
+    // props.s3Stack.bucket.grantReadWrite(lambdaFunction);
   }
 }
